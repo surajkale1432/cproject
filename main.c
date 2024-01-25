392c7069e8e73ee05e7ca21d68404fa69af7e3af
surajkale1432 <91897995+surajkale1432@users.noreply.github.com>
2024-01-25 11:49:46 +0530

    qq

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void main()
         printf("\nWelcome To Book Management System.\nPlease Select Your Choice.\n");
         printf("1. Add Book to Library.\n2. Display All Books Info.\n3. Search Books by ID\n4. Search Books by Name ");
         printf("\n5. Search Books by Author\n6. Search Books by Category.\n7. Remove Book from Library\n8. Total Books in Library\n9. Update Book Info\n10. Sort Books\n0. Exit");
-        printf("\nChoice = ");
+        printf("\n0. Exit \nChoice = ");
         fflush(stdin);
         if (scanf("%d", &ch))
         {
